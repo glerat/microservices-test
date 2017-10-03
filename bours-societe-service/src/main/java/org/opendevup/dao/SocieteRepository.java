aae54d6919d0632dd328e6c5d023cd2797034869
glerat <guy.lerat.jr@gmail.com>
2017-10-03 22:43:49 +0000

    add rest

--- bours-societe-service/src/main/java/org/opendevup/dao/SocieteRepository.java
+++ bours-societe-service/src/main/java/org/opendevup/dao/SocieteRepository.java
@@ -2,7 +2,9 @@ package org.opendevup.dao;
 
 import org.opendevup.entities.Societe;
 import org.springframework.data.jpa.repository.*;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource
 public interface SocieteRepository extends JpaRepository<Societe, Long> {
     
 }
